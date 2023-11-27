131e3ba0fcae9ab9b570220b885e2ae4190041ea
Monczak <37554206+Monczak@users.noreply.github.com>
2023-11-28 00:57:49 +0100

    Some random sync commit

--- Spelunker Unearthed/Scripts/Map Generation/Biomes/Biome.cs
+++ Spelunker Unearthed/Scripts/Map Generation/Biomes/Biome.cs
@@ -12,8 +12,8 @@ public class Biome : Resource<BiomeData>
     public string Name { get; private set; }
     public Color Color { get; private set; }
     
-    public ParameterProvider<Tile> WallTileProvider { get; private set; }
-    public ParameterProvider<Tile> GroundTileProvider { get; private set; }
+    public TileProvider WallTileProvider { get; private set; }
+    public TileProvider GroundTileProvider { get; private set; }
     
     protected override void BuildFromData(BiomeData data)
     {

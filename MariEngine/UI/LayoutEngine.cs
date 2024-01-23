28fa00c280bc365af2bb8cbd1453187e15efc6cf
Monczak <szymon.jedrzejczak.sj@gmail.com>
2024-01-23 18:02:05 +0100

    Refactor flex layout calculation

--- MariEngine/UI/LayoutEngine.cs
+++ MariEngine/UI/LayoutEngine.cs
@@ -39,82 +39,100 @@ public static class LayoutEngine
             LayoutNode layoutNode => UiMath.ApplyPadding(boundsMap[layoutNode], layoutNode.Padding),
             _ => boundsMap[node],
         };
-        
-        var totalFlexGrow = node.Children.Where(child => !child.HasPreferredSize).Sum(child => child.FlexGrow);
-        Vector2 error = Vector2.Zero;
 
         if (node is FlexLayoutNode flexLayoutNode)
         {
-            var flexDirection = flexLayoutNode.FlexDirection;
-            
-            Coord usableSize = usableBounds.Size;
-            for (int i = 0; i < flexLayoutNode.Children.Count; i++)
-            {
-                var child = flexLayoutNode.Children[i];
-                if (child.HasPreferredSize)
-                {
-                    usableSize -= flexDirection switch
-                    {
-                        FlexDirection.Row => Coord.UnitX * (child.PreferredWidth ?? 0),
-                        FlexDirection.Column => Coord.UnitY * (child.PreferredHeight ?? 0),
-                        _ => throw new ArgumentOutOfRangeException()
-                    }; 
-                    
-                    var (flexGapBefore, flexGapAfter) = CalculateFlexGap(flexLayoutNode, i);
-                    usableSize -= flexDirection switch
-                    {
-                        FlexDirection.Row => Coord.UnitX * (flexGapBefore + flexGapAfter),
-                        FlexDirection.Column => Coord.UnitY * (flexGapBefore + flexGapAfter),
-                        _ => throw new ArgumentOutOfRangeException()
-                    };
-                }
-            }
-            
-            Coord childPos = usableBounds.TopLeft;
-            for (int i = 0; i < flexLayoutNode.Children.Count; i++)
-            {
-                var child = flexLayoutNode.Children[i];
-                var (flexGapBefore, flexGapAfter) = CalculateFlexGap(flexLayoutNode, i);
+            PerformFlexLayout(depth, flexLayoutNode, usableBounds);
+        }
+    }
 
-                float flexGap = flexGapBefore + flexGapAfter;
+    private static void PerformFlexLayout(int depth, FlexLayoutNode flexLayoutNode, CoordBounds usableBounds)
+    {
+        var usableSize = AdjustForPreferredSizes(flexLayoutNode, usableBounds);
+        CalculateLayoutForChildren(depth, flexLayoutNode, usableBounds.TopLeft, usableSize);
+    }
+
+    private static void CalculateLayoutForChildren(int depth, FlexLayoutNode flexLayoutNode, Coord startPos,
+        Coord usableSize)
+    {
+        var flexDirection = flexLayoutNode.FlexDirection;
+        var totalFlexGrow = flexLayoutNode.Children.Where(child => !child.HasPreferredSize).Sum(child => child.FlexGrow);
+        
+        Vector2 error = Vector2.Zero;
+        Coord childPos = startPos;
+        for (int i = 0; i < flexLayoutNode.Children.Count; i++)
+        {
+            var child = flexLayoutNode.Children[i];
+            var (flexGapBefore, flexGapAfter) = CalculateFlexGap(flexLayoutNode, i);
+
+            float flexGap = flexGapBefore + flexGapAfter;
                 
-                Vector2 childSize = flexDirection switch
+            Vector2 childSize = flexDirection switch
+            {
+                FlexDirection.Row => new Vector2(
+                    child.PreferredWidth is not null ? (float)child.PreferredWidth : usableSize.X / totalFlexGrow * child.FlexGrow - flexGap,
+                    child.PreferredHeight is not null ? (float)child.PreferredHeight : usableSize.Y
+                ),
+                FlexDirection.Column => new Vector2(
+                    child.PreferredWidth is not null ? (float)child.PreferredWidth : usableSize.X,
+                    child.PreferredHeight is not null ? (float)child.PreferredHeight : usableSize.Y / totalFlexGrow * child.FlexGrow - flexGap
+                ),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+                
+            error += childSize - Vector2.Floor(childSize);
+            var roundedError = Vector2.Round(error);
+            childSize += roundedError;
+            error -= roundedError;
+                
+            childPos += flexDirection switch
+            {
+                FlexDirection.Row => Coord.UnitX * flexGapBefore,
+                FlexDirection.Column => Coord.UnitY * flexGapBefore,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+                
+            boundsMap[child] = new CoordBounds(childPos, (Coord)childSize);
+                
+            childPos += flexDirection switch
+            {
+                FlexDirection.Row => Coord.UnitX * (childSize.X + flexGapAfter),
+                FlexDirection.Column => Coord.UnitY * (childSize.Y + flexGapAfter),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+                
+            CalculateLayoutForNode(child, depth + 1);
+        }
+    }
+
+    private static Coord AdjustForPreferredSizes(FlexLayoutNode flexLayoutNode, CoordBounds usableBounds)
+    {
+        var flexDirection = flexLayoutNode.FlexDirection;
+        Coord usableSize = usableBounds.Size;
+
+        for (int i = 0; i < flexLayoutNode.Children.Count; i++)
+        {
+            var child = flexLayoutNode.Children[i];
+            if (child.HasPreferredSize)
+            {
+                usableSize -= flexDirection switch
                 {
-                    FlexDirection.Row => new Vector2(
-                        child.PreferredWidth is not null ? (float)child.PreferredWidth : usableSize.X / totalFlexGrow * child.FlexGrow - flexGap,
-                        child.PreferredHeight is not null ? (float)child.PreferredHeight : usableSize.Y
-                    ),
-                    FlexDirection.Column => new Vector2(
-                        child.PreferredWidth is not null ? (float)child.PreferredWidth : usableSize.X,
-                        child.PreferredHeight is not null ? (float)child.PreferredHeight : usableSize.Y / totalFlexGrow * child.FlexGrow - flexGap
-                    ),
+                    FlexDirection.Row => Coord.UnitX * (child.PreferredWidth ?? 0),
+                    FlexDirection.Column => Coord.UnitY * (child.PreferredHeight ?? 0),
+                    _ => throw new ArgumentOutOfRangeException()
+                }; 
+                    
+                var (flexGapBefore, flexGapAfter) = CalculateFlexGap(flexLayoutNode, i);
+                usableSize -= flexDirection switch
+                {
+                    FlexDirection.Row => Coord.UnitX * (flexGapBefore + flexGapAfter),
+                    FlexDirection.Column => Coord.UnitY * (flexGapBefore + flexGapAfter),
                     _ => throw new ArgumentOutOfRangeException()
                 };
-                
-                error += childSize - Vector2.Floor(childSize);
-                var roundedError = Vector2.Round(error);
-                childSize += roundedError;
-                error -= roundedError;
-                
-                childPos += flexDirection switch
-                {
-                    FlexDirection.Row => Coord.UnitX * flexGapBefore,
-                    FlexDirection.Column => Coord.UnitY * flexGapBefore,
-                    _ => throw new ArgumentOutOfRangeException()
-                };
-                
-                boundsMap[child] = new CoordBounds(childPos, (Coord)childSize);
-                
-                childPos += flexDirection switch
-                {
-                    FlexDirection.Row => Coord.UnitX * (childSize.X + flexGapAfter),
-                    FlexDirection.Column => Coord.UnitY * (childSize.Y + flexGapAfter),
-                    _ => throw new ArgumentOutOfRangeException()
-                };
-                
-                CalculateLayoutForNode(child, depth + 1);
             }
         }
+
+        return usableSize;
     }
 
     private static (float flexGapBefore, float flexGapAfter) CalculateFlexGap(FlexLayoutNode node, int indexInParent)
